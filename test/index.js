f8b3939d2dbc4180c34cee0a8bd4989557d84323
galkinrost <galkinrost@gmail.com>
2014-07-30 17:44:05 +0400

    create 'expect' method

--- test/index.js
+++ test/index.js
@@ -1,17 +1,24 @@
 var chai = require('chai');
 var esprima = require('esprima');
+var path = require('path');
 var fs = require('fs');
 var parser = require('../lib/parser');
+
+function expect(obj, fixtureFilename) {
+    var expected = fs.readFileSync(path.join(__dirname, 'expects', fixtureFilename), 'utf8');
+    expected = expected.replace(/(\r\n|\n|\r|\s)/gm, '');
+    chai.expect(JSON.stringify(obj)).to.equal(expected);
+}
 
 describe('Parser', function () {
 
     it('#moduleParser', function () {
         var str = fs.readFileSync(__dirname + '/fixture/module.js', 'utf8');
-        var expect = fs.readFileSync(__dirname + '/expects/parser/module.js', 'utf8');
 
         var ast = esprima.parse(str);
 
         var result = parser.moduleParser(ast);
-        chai.expect(JSON.stringify(result)).to.equal(expect);
+
+        expect(result, 'module.js');
     });
 });
